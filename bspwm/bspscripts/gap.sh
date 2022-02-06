3f4ae3653be1ca0bc9a0f41f429903c08ff4e90e
Jonathan Svensson <racingjss@hotmail.com>
2022-02-06 00:08:42 -0500

    initialize dotfiles

---
+++ bspwm/bspscripts/gap.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Credit: Hibiki @ https://gitgud.io/hibiki/dotfiles/blob/master/bspwm/.config/bspwm/bspscripts/gap.sh
+case "$1" in
+	tog)
+		if [ 0 -lt $((`bspc config window_gap`)) ]; then
+			bspc config window_gap 0
+		else
+			bspc config window_gap 12
+		fi
+		;;
+	dec)
+		if [ 0 -ne $((`bspc config window_gap`)) ]; then
+			bspc config window_gap $(( `bspc config -d focused window_gap` - 3))
+		fi
+		;;
+	inc)
+		bspc config window_gap $(( `bspc config -d focused window_gap` + 3))
+		;;
+esac
+
